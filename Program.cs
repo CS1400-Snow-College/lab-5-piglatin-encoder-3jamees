8d0b4ccd5567eea33c0202c89fe76d1d79564342
3jamees <jamesiiking8@gmail.com>
2025-07-02 18:59:37 -0400

    greeting inserted

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Name: James King II
+// Date: July 1, 2025
+// Lab: Lab 5 piglatin-encoder
+
+ Console.WriteLine("Welcome! This program changes your message to Pig Latin and then hides it using a secret code.");
